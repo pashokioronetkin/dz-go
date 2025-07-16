8ae86f6c75c52d940d68dcc8b94964f45632ba50
donskikh <donskihpavel8@gmail.com>
2025-07-16 15:28:54 +0300

    Test 3. New func: getInputCurrency(), getOutputCurrency(string), getQuantity(), calculate(string, string, float64)

--- main.go
+++ main.go
@@ -1,36 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const USDtoEURO float64 = 0.86
 const USDtoRUB float64 = 77.89
-const EUROtoRUB float64 = USDtoRUB / USDtoEURO
+const RUBtoUSD float64 = 0.013
+const RUBtoEURO float64 = 0.011
+const EUROtoUSD float64 = 1.16
+const EUROtoRUB float64 = 91.15
 
-func getUserInput() (string, float64, string) {
-	var quantity float64
-	var currency1, currency2 string
+func getInputCurrency() string {
+	var inputCurrency string
+	fmt.Print("Введите исходную валюту (USD, EURO, RUB): ")
+	for {
+		fmt.Scan(&inputCurrency)
 
-	fmt.Print("Введите исходную валюту: ")
-	fmt.Scan(&currency1)
+		if inputCurrency == "RUB" || inputCurrency == "USD" || inputCurrency == "EURO" {
+			return inputCurrency
+		}
+		fmt.Print("Неверно выбрана валюта\nВведите заново: ")
+	}
+}
 
-	fmt.Print("Введите количество: ")
-	fmt.Scan(&quantity)
+func getOutputCurrency(inputCurrency string) string {
+	var outputCurrency string
+	for {
+		if inputCurrency == "RUB" {
+			fmt.Print("Введите целевую валюту (USD, EURO): ")
+			fmt.Scan(&outputCurrency)
+			if outputCurrency == "USD" || outputCurrency == "EURO" {
+				return outputCurrency
+			}
+		} else if inputCurrency == "USD" {
+			fmt.Print("Введите целевую валюту (RUB, EURO): ")
+			fmt.Scan(&outputCurrency)
+			if outputCurrency == "RUB" || outputCurrency == "EURO" {
+				return outputCurrency
+			}
+		} else if inputCurrency == "EURO" {
+			fmt.Print("Введите целевую валюту (USD, RUB): ")
+			fmt.Scan(&outputCurrency)
+			if outputCurrency == "USD" || outputCurrency == "RUB" {
+				return outputCurrency
+			}
+		}
+	}
+}
 
-	fmt.Print("Введите целевую валюту: ")
-	fmt.Scan(&currency2)
-
-	return currency1, quantity, currency2
+func getQuantity() float64 {
+	var inputCurrencyQuantity float64
+	for {
+		fmt.Print("Введите количество исходной валюты: ")
+		_, err := fmt.Scan(&inputCurrencyQuantity)
+		if err != nil {
+			fmt.Println("Ошибка: введено не число! Введите количество заново")
+			var discard string
+			fmt.Scanln(&discard)
+			continue
+		} else {
+			return inputCurrencyQuantity
+		}
+	}
 }
 
 func calculate(currency1 string, currency2 string, quantity float64) float64 {
-	return USDtoEURO / quantity
+	fmt.Println("Конвертация из", currency1, "в", currency2)
+
+	if currency1 == "USD" && currency2 == "EURO" {
+		return USDtoEURO * quantity
+	} else if currency1 == "USD" && currency2 == "RUB" {
+		return USDtoRUB * quantity
+	}
+
+	if currency1 == "RUB" && currency2 == "USD" {
+		return RUBtoUSD * quantity
+	} else if currency1 == "RUB" && currency2 == "EURO" {
+		return RUBtoEURO * quantity
+	}
+
+	if currency1 == "EURO" && currency2 == "RUB" {
+		return EUROtoRUB * quantity
+	} else if currency1 == "EURO" && currency2 == "USD" {
+		return EUROtoUSD * quantity
+	} else {
+		return 0
+	}
 }
 
 func main() {
-	currency1, quantity, currency2 := getUserInput()
+	inputCurrency := getInputCurrency()
+	outputCurrency := getOutputCurrency(inputCurrency)
 
-	exchange := calculate(currency1, currency2, quantity)
-
-	fmt.Print(currency1, " ", quantity, " ", currency2, '\n')
-	fmt.Printf("%.2f", exchange)
+	result := calculate(inputCurrency, outputCurrency, getQuantity())
+	fmt.Print(result)
 }
